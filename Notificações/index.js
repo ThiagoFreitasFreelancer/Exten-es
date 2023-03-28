ce13521e258eabe3f773b349390ab54352e34a80
Thiago Freitas <thiago.goncalves@cepedi.org.br>
2023-03-28 15:41:55 -0300

    alterações

--- Notificações/index.js
+++ Notificações/index.js
@@ -1,5 +1,48 @@
 
 
 
-button = document.querySelectorAll("#btn");
+// const button = document.querySelector('.button');
+
+// //console.log(button)
+
+// // button.addEventListener('click', (e) => {
+
+// //     let textArea = document.querySelector( '.text' );
+// //     textArea.select();
+// //     window.open(text, '_blank');
+
+// // });
+
+// document.getElementById("btn").addEventListener("click", function() {
+
+//     var conteudo = document.querySelector("textarea[class='text']").value;
+//     this.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(conteudo));
+//     this.setAttribute("download", "conteudo.txt");
+
+//     const downloadLink = document.createElement("a");
+//     document.body.appendChild(downloadLink);
+//     const blob = new Blob(conteudo)
+//     downloadLink.href = URL.createObjectURL(blob);
+//     downloadLink.download = "conteudo.txt";
+//     downloadLink.click();
+//     URL.revokeObjectURL(downloadLink.href);
+
+// });
+
+const textArea = document.querySelector(".text");
+const downloadLink = document.createElement("a");
+downloadLink.hidden = true;
+document.body.appendChild(downloadLink);
+
+document.querySelector(".button").addEventListener("click", () => {
+  
+    const blob = new Blob([textArea.value], { type: "text/plain;charset=utf-8" });
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.download = "conteudo.txt";
+    downloadLink.click();
+    URL.revokeObjectURL(downloadLink.href);
+
+});
+
+
 
